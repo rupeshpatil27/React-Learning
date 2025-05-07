6d290c2b8690834ec2ad93ad856c534e745f9b26
rupeshpatil <rupeshpatil88927@gmail.com>
2025-05-07 21:34:25 +0530

    uncontrolled component example

--- uncontrolled-component/src/App.jsx
+++ uncontrolled-component/src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [textval, setTextval] = useState("")
+
+  function handleSubmit(e) {
+
+    e.preventDefault()
+const txt=document.getElementById("name").value
+setTextval(txt)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <h1>Uncontrolled Components</h1>
+
+        <input type='text' name='name' id='name' placeholder='Enter text' />
+        <br />
+        <br />
+
+        {textval}
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
     </>
   )
 }
