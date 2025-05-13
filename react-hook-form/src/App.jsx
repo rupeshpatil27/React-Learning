2c04fe2efd7ffbcbb9f36ca16185d5245422ecf0
rupeshpatil <rupeshpatil88927@gmail.com>
2025-05-13 23:41:55 +0530

    react hook form input validation

--- react-hook-form/src/App.jsx
+++ react-hook-form/src/App.jsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 
 function App() {
 
-  let text="";
+  let text = "";
 
-    const { register, handleSubmit,getValues } = useForm({
+  const { register, handleSubmit, getValues, formState: { errors } } = useForm({
     defaultValues: {
       firstName: "",
       lastName: "",
@@ -19,12 +19,26 @@ function App() {
 
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)} className="main-container">
-      <input {...register("firstName")}  className="input"/><br/>
-      <input {...register("lastName")}  className="input"/><br/>
-      <input type="submit" value="Submit" className="input-btn"/><br/>
-    </form>
-</>
+      <form onSubmit={handleSubmit(onSubmit)} className="main-container">
+        <input {...register("firstName", { required: true, maxLength: 30 })} placeholder="Enter Name" className="input" /><br />
+
+        {errors.firstName && errors.firstName.type === "required" && (
+          <span role="alert" className="error-message">This is required</span>
+        )}
+
+        {errors.firstName && errors.firstName.type === "maxLength" && (
+          <span role="alert" className="error-message">Max length exceeded</span>
+        )}
+
+        <input {...register("lastName",{ required: true})} placeholder="Enter Mobile" className="input" /><br />
+
+         {errors.lastName && errors.lastName.type === "required" && (
+          <span role="alert" className="error-message">This is required</span>
+        )}
+
+        <input type="submit" value="Submit" className="input-btn" /><br />
+      </form>
+    </>
   )
 }
 
