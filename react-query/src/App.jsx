375cd3ff2cbe54ffcde27d1715d225935e6bc5fa
rupeshpatil <rupeshpatil88927@gmail.com>
2025-05-17 23:30:59 +0530

    explore react useQuery properties

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -1,27 +1,13 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import {
-  useQuery
-} from '@tanstack/react-query'
-import { fetchData } from './api/user'
+import User from './component/user'
 
 function App() {
 
-  // const [users, setUsers] = useState([])
-  // function showData() {
-  const { data } = useQuery({ queryKey: ['users'], queryFn: fetchData })
-  // setUsers(data)
-  console.log(data)
-  // }
-  // })
 
   return (
     <div className="main-container" style={{ overflowX: "auto" }}>
       <h1>React Query</h1>
-      {/* <button onClick={showData}>show user</button> */}
-      {/* {data?.users.map((item, index) => (
-        <h3>{item.firstName}</h3>
-      ))} */}
+      <User />
     </div>
   )
 }
---
+++ react-query/src/component/User.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchData } from "../api/user";
+
+const User = () => {
+  const {
+    data: userData,
+    isLoading,
+    isError,
+  } = useQuery({ queryKey: ["users"], queryFn: fetchData(), enabled: true });
+  return (
+    <div>
+      {isLoading && "Loading...."}
+      {isError?.message && isError?.message}
+
+      {userData?.users?.map((item, index) => (
+        <div className="card" key={index}>
+          <span className="txt">{item.firstName}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default User;
