d68da8ec04765b46fb74df8ffad0d3a0b4b6e623
rupeshpatil <rupeshpatil88927@gmail.com>
2025-06-09 23:53:27 +0530

    useMemo example ui

---
+++ usememo/src/App.jsx
@@ -0,0 +1,21 @@
+import { useCallback, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+      <div className='h-[60%] w-[60%] p-[15] bg-gray-200 flex flex-col items-center justify-center gap-6'>
+
+        <h1 className='text-4xl font-extrabold'>useMemo</h1>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default App
