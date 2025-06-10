dd214caddfb7399c54a7ed7e9e748e30aa2fd15a
rupeshpatil <rupeshpatil88927@gmail.com>
2025-06-10 23:58:16 +0530

    add useMemo example

--- usememo/src/App.jsx
+++ usememo/src/App.jsx
@@ -1,10 +1,23 @@
-import { useCallback, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useMemo, useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [count, setCount] = useState(0)
+  const [input, setInput] = useState(0)
+
+
+  function startCount() {
+    setCount((cn) => cn + 1)
+  }
+
+  function expensiveExp(num) {
+    for (var i = 0; i < 1000000000; i++) { }
+    return num * 2
+  }
+
+
+  const doubleValue = useMemo(() => expensiveExp(input), [input])
+
 
   return (
     <div className='h-screen w-full flex items-center justify-center'>
@@ -13,6 +26,14 @@ function App() {
         <h1 className='text-4xl font-extrabold'>useMemo</h1>
 
 
+        <div className='flex items-center justify-center gap-5'>
+          {/* <span className='text-2xl font-normal'>Pressed</span> : <span>{pressedCount}</span> */}
+          <span className='text-2xl font-normal'> Normal Count</span> : <span>{count}</span>
+          <span className='text-2xl font-normal'>double</span> : <span>{doubleValue}  (Expensive Expresion run using UseMeme hook)</span>
+        </div>
+
+        <input type='number' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Enter number' className='bg-amber-200 px-10 py-4' />
+        <button className='px-10 py-4 bg-blue-500 rounded-3xl' onClick={startCount}>Trigger</button>
       </div>
     </div>
   )
