c112627551ca25cb02a6b556346e48af00c00610
rupeshpatil <rupeshpatil88927@gmail.com>
2025-06-11 23:47:18 +0530

    contextapi project  ui

---
+++ contextApi/src/App.jsx
@@ -0,0 +1,21 @@
+import { useMemo, useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+      <div className='h-full w-full p-[20] bg-neutral-300 flex flex-col items-center justify-center gap-6'>
+
+        <h1 className='text-4xl font-extrabold'>useMemo</h1>
+
+
+        <div className='flex items-center justify-center gap-5'>
+         
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
