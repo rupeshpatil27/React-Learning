653bdfb44917b84a9f7b87d5776c1e84cfb35646
rupeshpatil <rupeshpatil88927@gmail.com>
2025-06-15 23:55:00 +0530

    ui design

---
+++ reactRedux/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+      <div className='h-screen w-full flex items-center justify-center'>
+        <div className='h-full w-full p-[20] bg-neutral-300 flex flex-col items-center gap-6'>
+
+          <h1 className='text-4xl font-extrabold'>Redux</h1>
+
+
+          <div className='flex items-center justify-center gap-5'>
+           
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default App
