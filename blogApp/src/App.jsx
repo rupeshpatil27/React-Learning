ae6199398c8514ace5ec452242cd9ab97f72083f
rupeshpatil <rupeshpatil88927@gmail.com>
2025-06-21 23:45:06 +0530

    Redux Toolkit - start blog app

---
+++ blogApp/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <div className='h-full w-full'>
+      <h1 className='text-4xl font-extrabold text-center'>Blogs</h1>
+
+      <div className="w-full h-full flex justify-center items-center flex-col gap-3 mt-15">
+      </div>
+    </div>
+  )
+}
+
+export default App
