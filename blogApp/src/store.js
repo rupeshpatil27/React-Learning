f8f1a89de3b46472ed73a3c8aba701ff0c4f9d40
rupeshpatil <rupeshpatil88927@gmail.com>
2025-07-05 23:58:15 +0530

    update files

--- blogApp/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import blogReducer from "./features/blog/blogSlice";
-
-export const store = configureStore({
-  reducer: {
-    blog:blogReducer,
-  },
-});
