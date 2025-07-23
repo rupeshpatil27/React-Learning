4046227c4c371a47b00726eea9b45499198ae50a
rupeshpatil <rupeshpatil88927@gmail.com>
2025-07-23 23:57:03 +0530

    start spline galaxy 3d model project

---
+++ spline-galaxy-3d-model/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className='bg-[#030014] overflow-y-scroll overflow-x-hidden h-screen relative'>
+      <div className='h-full w-full text-white'>
+spline
+      </div>
+
+    </div>
+  )
+}
+
+export default App
