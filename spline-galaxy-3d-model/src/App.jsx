07fe5bbae25c7bff5d27008a3ec52e04d9b21817
rupeshpatil <rupeshpatil88927@gmail.com>
2025-07-24 00:09:52 +0530

    add spline galaxy 3d model

--- spline-galaxy-3d-model/src/App.jsx
+++ spline-galaxy-3d-model/src/App.jsx
@@ -1,13 +1,15 @@
+import StarsCanvas from "./components/StarBackground";
+import Spline from "@splinetool/react-spline";
+
 function App() {
-
   return (
-    <div className='bg-[#030014] overflow-y-scroll overflow-x-hidden h-screen relative'>
-      <div className='h-full w-full text-white'>
-spline
+    <div className="bg-[#030014] overflow-y-scroll overflow-x-hidden h-screen relative">
+      <StarsCanvas />
+      <div className="h-full w-full text-white">
+        <Spline scene="https://prod.spline.design/NIY6Evv2TH3yzF12/scene.splinecode" />
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
