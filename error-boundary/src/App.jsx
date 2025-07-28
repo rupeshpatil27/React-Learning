222017b380e8e50fda0e768c6e91ce644ef89a42
rupeshpatil <rupeshpatil88927@gmail.com>
2025-07-28 23:28:06 +0530

    Start learning about Error Boundaries

---
+++ error-boundary/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+      <div className='h-[60%] w-[60%] p-[15] bg-gray-200 flex flex-col items-center justify-center gap-6'>
+
+        <h1 className='text-4xl font-extrabold'>Error Boundary</h1>
+
+
+        <div className='flex items-center justify-center gap-5'>
+          
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
