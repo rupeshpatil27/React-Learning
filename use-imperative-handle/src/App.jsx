c0f123594fbce80615bc658094f5398eb912f521
rupeshpatil <rupeshpatil88927@gmail.com>
2025-08-01 23:22:53 +0530

    Use CustomInput with useImperativeHandle in App component

--- use-imperative-handle/src/App.jsx
+++ use-imperative-handle/src/App.jsx
@@ -1,11 +1,31 @@
+import { useRef } from "react";
+import CustomInput from "./component/CustomInput ";
+
 function App() {
+
+  const inputRef = useRef();
+
+  const handleReset = () => {
+    inputRef.current.reset();
+  };
 
   return (
     <div className='h-screen w-full flex items-center justify-center'>
-      <div className='h-[60%] w-[60%] bg-red-200 rounded-2xl flex flex-col items-center justify-start'>
+      <div className='h-[60%] w-[60%] bg-red-200 rounded-2xl flex flex-col items-center justify-start gap-2'>
 
         <h1 className='text-4xl font-extrabold mt-2'>useImperativeHandle</h1>
 
+      <div className="mt-10">
+          <CustomInput ref={inputRef} />
+      </div>
+        <div className="space-x-4">
+          <button
+            onClick={handleReset}
+            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   )
--- use-imperative-handle/src/component/CustomInput .jsx
+++ use-imperative-handle/src/component/CustomInput .jsx
@@ -4,15 +4,13 @@ const CustomInput = (props, ref) => {
     const inputRef = useRef();
 
     useImperativeHandle(ref, () => ({
-        focus: () => {
-            inputRef.current.focus();
-        },
         reset: () => {
             inputRef.current.value = "";
+            inputRef.current.focus();
         }
     }));
 
-    return <input ref={inputRef} type="text" placeholder="Type here..." />;
+    return <input ref={inputRef} type="text" placeholder="Type here..." className="border p-2 rounded w-full" />;
 };
 
 export default forwardRef(CustomInput);
