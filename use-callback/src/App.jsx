f17ed2cf0e84f60c5e8bb26bf50d3a701e7967ac
rupeshpatil <rupeshpatil88927@gmail.com>
2025-08-04 00:16:24 +0530

    add shuffling and search list functionality

--- use-callback/src/App.jsx
+++ use-callback/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import SearchInput from "./component/SearchInput";
 
 const initialUsers = ["Alice", "Bob", "Charlie", "David", "Eva"];
 function App() {
   const [users, setUsers] = useState(initialUsers);
-  const [searchTerm, setSearchTerm] = useState("");
 
+  const handleSearch = (text) => {
+    const filteredUsers = users.filter((user) =>
+      user.toLowerCase().includes(text.toLowerCase())
+    );
+    setUsers(text ? filteredUsers : initialUsers);
+  };
 
   // Shuffle users
   const shuffleUsers = () => {
@@ -14,18 +20,28 @@ function App() {
 
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <div className="h-[60%] w-[60%] bg-red-100 rounded-2xl flex flex-col items-center justify-start gap-2">
+      <div className="h-[80%] w-[60%] bg-red-100 rounded-2xl flex flex-col items-center justify-start gap-2">
         <h1 className="text-4xl font-extrabold mt-2">useCallback</h1>
 
-        <div className="space-x-4">
-          <button onClick={shuffleUsers} className="bg-blue-400 py-2 px-2 rounded-xl text-white font-semibold">
+        <div className="space-x-4 flex flex-row gap-5 items-center my-5">
+          <button
+            onClick={shuffleUsers}
+            className="bg-blue-500 text-white py-2 px-6 rounded-lg shadow-lg hover:bg-blue-600 focus:outline-none transition-all"
+          >
             Shuffle
           </button>
+
+          <SearchInput onChange={handleSearch} />
         </div>
 
-        <ul>
-          {users.map((user,i) => (
-            <li key={i}>{user}</li>
+        <ul className="list-none p-0 w-64 mb-6">
+          {users.map((user, i) => (
+            <li
+              key={i}
+              className="bg-white p-4 mb-2 rounded-lg shadow-lg hover:bg-blue-50 transition-all"
+            >
+              {user}
+            </li>
           ))}
         </ul>
       </div>
--- use-callback/src/component/SearchInput.jsx
+++ use-callback/src/component/SearchInput.jsx
@@ -9,6 +9,7 @@ const SearchInput = ({ onChange }) => {
         type="text"
         onChange={(e) => onChange(e.target.value)}
         placeholder="Search users..."
+        className="px-4 py-2 w-64 border border-gray-300 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </div>
   );
