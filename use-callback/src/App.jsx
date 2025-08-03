727a245c5a66c44c721edc533a92dc85b7914ee0
rupeshpatil <rupeshpatil88927@gmail.com>
2025-08-03 23:40:21 +0530

    start learning useCallback hook

---
+++ use-callback/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+      <div className='h-[60%] w-[60%] bg-red-100 rounded-2xl flex flex-col items-center justify-start gap-2'>
+
+        <h1 className='text-4xl font-extrabold mt-2'>useCallback</h1>
+
+        <div className="space-x-4">
+        
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
