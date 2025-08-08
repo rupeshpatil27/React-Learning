3de6a9f2aebacefb3191bda969f34c800ae742b1
rupeshpatil <rupeshpatil88927@gmail.com>
2025-08-08 23:53:45 +0530

    file input project setup

---
+++ file-input-with-progress-bar/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <div className='h-screen w-full flex items-center justify-center gap-2 p-10'>
+      <div className='h-full w-full p-[15] bg-gray-200 rounded-2xl'>
+
+        <h1 className='text-4xl font-extrabold text-center my-5'>File Upload</h1>
+
+        
+
+      </div>
+    </div>
+  )
+}
+
+export default App
